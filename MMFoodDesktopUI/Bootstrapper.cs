63f3abcc6153d702456115b2870e3daef6583e84
Sergiu Crisan <CDSergiu@outlook.com>
2022-04-21 17:10:25 +0300

    Created the LoginView
    Created and wired up with the LoginViewModel.

--- MMFoodDesktopUI/Bootstrapper.cs
+++ MMFoodDesktopUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using MMFoodDesktopUI.Models;
 using MMFoodDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace MMFoodDesktopUI
 {
@@ -22,6 +24,11 @@ namespace MMFoodDesktopUI
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
 
         /// <summary>
---
+++ MMFoodDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMFoodDesktopUI.ViewModels
+{
+    public class LoginViewModel: Screen
+    {
+        private string _username = "";
+        private string _password;
+
+        public string Username
+        {
+            get { return _username; }
+            set 
+            {
+                _username = value;
+                NotifyOfPropertyChange(()=> Username );
+                NotifyOfPropertyChange(() => CanLogin);               
+            }
+        }    
+
+        public string Password
+        {
+            get { return _password; }
+            set 
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogin);                
+            }
+        }
+
+        public bool CanLogin { 
+            get
+            {
+                bool output = false;
+
+                if (Username.Length > 0 && Password.Length > 6)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+        }
+
+        public void Login(string username, string password)
+        {
+            Console.WriteLine();
+        }
+
+
+
+    }
+}
